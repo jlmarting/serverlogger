b7b7cecfd7741bbe74aa172fafde1cd40d6f1805
josh <jlmartingalvan@gmail.com>
2014-12-17 17:34:26 +0100

    Servidor reescrito a ExpressJS (codigo anterior comentado)

--- serverlogger/public_html/server.js
+++ serverlogger/public_html/server.js
@@ -1,4 +1,6 @@
-var http=require("http");
+//var http=require("http");
+var express =  require("express");
+var app = express();
 var fs=require("fs");
 var socket = require("socket.io");
 
@@ -6,7 +8,7 @@ var port = 9911;
 
 var bufferMsg = new Array;
 
-var httpServer = http.createServer (function(req,res){   
+/*var httpServer = http.createServer (function(req,res){   
        
     console.log('solicitud: ' + req.url);
     fs.readFile('.'+req.url,function(err,html){
@@ -21,11 +23,27 @@ var httpServer = http.createServer (function(req,res){
         });
        
     }).listen(port);
+    */
+   
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});  
+
+app.use(express.static(__dirname));
+
+app.get('/', function(req,res){
+    res.send('Server is working hard! :P');
+    res.end();    
+});
+
+var server = app.listen(port);
 
 console.log('Servidor web en marcha en ' + port);
 
-var io = socket.listen(httpServer);
-
+//var io = socket.listen(httpServer);
+var io = socket.listen(server);
 io.sockets.on('connection', function(socket){
     console.log('Cliente conectado: ' + socket.id);
 
